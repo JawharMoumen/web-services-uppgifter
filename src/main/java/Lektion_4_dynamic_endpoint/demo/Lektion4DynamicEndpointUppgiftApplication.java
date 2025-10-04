7e2b9a7a09f603554897d5d795771792f2686556
JawharMoumen <jawhar.moumen@stud.sti.se>
2025-10-05 00:37:06 +0200

    Flyttat alla filer till rätt mapp och lagt till nya klasser

---
+++ src/main/java/Lektion_4_dynamic_endpoint/demo/Lektion4DynamicEndpointUppgiftApplication.java
@@ -0,0 +1,13 @@
+package Lektion_4_dynamic_endpoint.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lektion4DynamicEndpointUppgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lektion4DynamicEndpointUppgiftApplication.class, args);
+	}
+
+}
